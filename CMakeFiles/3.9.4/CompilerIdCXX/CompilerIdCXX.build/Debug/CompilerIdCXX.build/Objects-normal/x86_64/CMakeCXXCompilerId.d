5040f4c83eb5fceb60978f5b56d40b93ecfc1130
njligames <jamesfolk1@gmail.com>
2017-12-14 01:41:38 -0500

    ignoring necessary files

--- CMakeFiles/3.9.4/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/CMakeFiles/3.9.4/CompilerIdCXX/CMakeCXXCompilerId.cpp
--- cmake_install.cmake
+++ cmake_install.cmake
@@ -29,35 +29,35 @@ endif()
 
 if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework" TYPE DIRECTORY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Debug/NJLI.framework" USE_SOURCE_PERMISSIONS)
-    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework/NJLI.framework/NJLI" AND
-       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework/NJLI.framework/NJLI")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework" TYPE DIRECTORY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Debug/NJLI.framework" USE_SOURCE_PERMISSIONS)
+    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework/NJLI.framework/NJLI" AND
+       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework/NJLI.framework/NJLI")
       if(CMAKE_INSTALL_DO_STRIP)
-        execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework/NJLI.framework/NJLI")
+        execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework/NJLI.framework/NJLI")
       endif()
     endif()
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework" TYPE DIRECTORY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Release/NJLI.framework" USE_SOURCE_PERMISSIONS)
-    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework/NJLI.framework/NJLI" AND
-       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework/NJLI.framework/NJLI")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework" TYPE DIRECTORY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Release/NJLI.framework" USE_SOURCE_PERMISSIONS)
+    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework/NJLI.framework/NJLI" AND
+       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework/NJLI.framework/NJLI")
       if(CMAKE_INSTALL_DO_STRIP)
-        execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework/NJLI.framework/NJLI")
+        execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework/NJLI.framework/NJLI")
       endif()
     endif()
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework" TYPE DIRECTORY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/MinSizeRel/NJLI.framework" USE_SOURCE_PERMISSIONS)
-    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework/NJLI.framework/NJLI" AND
-       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework/NJLI.framework/NJLI")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework" TYPE DIRECTORY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/MinSizeRel/NJLI.framework" USE_SOURCE_PERMISSIONS)
+    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework/NJLI.framework/NJLI" AND
+       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework/NJLI.framework/NJLI")
       if(CMAKE_INSTALL_DO_STRIP)
-        execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework/NJLI.framework/NJLI")
+        execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework/NJLI.framework/NJLI")
       endif()
     endif()
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework" TYPE DIRECTORY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/RelWithDebInfo/NJLI.framework" USE_SOURCE_PERMISSIONS)
-    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework/NJLI.framework/NJLI" AND
-       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework/NJLI.framework/NJLI")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework" TYPE DIRECTORY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/RelWithDebInfo/NJLI.framework" USE_SOURCE_PERMISSIONS)
+    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework/NJLI.framework/NJLI" AND
+       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework/NJLI.framework/NJLI")
       if(CMAKE_INSTALL_DO_STRIP)
-        execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/framework/NJLI.framework/NJLI")
+        execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/framework/NJLI.framework/NJLI")
       endif()
     endif()
   endif()
@@ -65,28 +65,28 @@ endif()
 
 if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static" TYPE STATIC_LIBRARY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Debug/libnjli.a")
-    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static/libnjli.a" AND
-       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static/libnjli.a")
-      execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ranlib" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static/libnjli.a")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static" TYPE STATIC_LIBRARY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Debug/libnjli.a")
+    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static/libnjli.a" AND
+       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static/libnjli.a")
+      execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ranlib" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static/libnjli.a")
     endif()
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static" TYPE STATIC_LIBRARY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Release/libnjli.a")
-    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static/libnjli.a" AND
-       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static/libnjli.a")
-      execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ranlib" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static/libnjli.a")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static" TYPE STATIC_LIBRARY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Release/libnjli.a")
+    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static/libnjli.a" AND
+       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static/libnjli.a")
+      execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ranlib" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static/libnjli.a")
     endif()
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static" TYPE STATIC_LIBRARY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/MinSizeRel/libnjli.a")
-    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static/libnjli.a" AND
-       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static/libnjli.a")
-      execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ranlib" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static/libnjli.a")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static" TYPE STATIC_LIBRARY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/MinSizeRel/libnjli.a")
+    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static/libnjli.a" AND
+       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static/libnjli.a")
+      execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ranlib" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static/libnjli.a")
     endif()
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static" TYPE STATIC_LIBRARY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/RelWithDebInfo/libnjli.a")
-    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static/libnjli.a" AND
-       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static/libnjli.a")
-      execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ranlib" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/static/libnjli.a")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static" TYPE STATIC_LIBRARY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/RelWithDebInfo/libnjli.a")
+    if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static/libnjli.a" AND
+       NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static/libnjli.a")
+      execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ranlib" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/static/libnjli.a")
     endif()
   endif()
 endif()
@@ -97,15 +97,15 @@ endif()
 
 if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib" TYPE SHARED_LIBRARY FILES
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib" TYPE SHARED_LIBRARY FILES
       "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Debug/libnjli.1.0.0.dylib"
       "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Debug/libnjli.1.dylib"
       "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Debug/libnjli.dylib"
       )
     foreach(file
-        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/libnjli.1.0.0.dylib"
-        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/libnjli.1.dylib"
-        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/libnjli.dylib"
+        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/libnjli.1.0.0.dylib"
+        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/libnjli.1.dylib"
+        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/libnjli.dylib"
         )
       if(EXISTS "${file}" AND
          NOT IS_SYMLINK "${file}")
@@ -115,15 +115,15 @@ if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMP
       endif()
     endforeach()
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib" TYPE SHARED_LIBRARY FILES
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib" TYPE SHARED_LIBRARY FILES
       "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Release/libnjli.1.0.0.dylib"
       "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Release/libnjli.1.dylib"
       "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Release/libnjli.dylib"
       )
     foreach(file
-        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/libnjli.1.0.0.dylib"
-        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/libnjli.1.dylib"
-        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/libnjli.dylib"
+        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/libnjli.1.0.0.dylib"
+        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/libnjli.1.dylib"
+        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/libnjli.dylib"
         )
       if(EXISTS "${file}" AND
          NOT IS_SYMLINK "${file}")
@@ -133,15 +133,15 @@ if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMP
       endif()
     endforeach()
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib" TYPE SHARED_LIBRARY FILES
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib" TYPE SHARED_LIBRARY FILES
       "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/MinSizeRel/libnjli.1.0.0.dylib"
       "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/MinSizeRel/libnjli.1.dylib"
       "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/MinSizeRel/libnjli.dylib"
       )
     foreach(file
-        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/libnjli.1.0.0.dylib"
-        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/libnjli.1.dylib"
-        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/libnjli.dylib"
+        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/libnjli.1.0.0.dylib"
+        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/libnjli.1.dylib"
+        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/libnjli.dylib"
         )
       if(EXISTS "${file}" AND
          NOT IS_SYMLINK "${file}")
@@ -151,15 +151,15 @@ if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMP
       endif()
     endforeach()
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib" TYPE SHARED_LIBRARY FILES
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib" TYPE SHARED_LIBRARY FILES
       "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/RelWithDebInfo/libnjli.1.0.0.dylib"
       "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/RelWithDebInfo/libnjli.1.dylib"
       "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/RelWithDebInfo/libnjli.dylib"
       )
     foreach(file
-        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/libnjli.1.0.0.dylib"
-        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/libnjli.1.dylib"
-        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/lib/libnjli.dylib"
+        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/libnjli.1.0.0.dylib"
+        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/libnjli.1.dylib"
+        "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/lib/libnjli.dylib"
         )
       if(EXISTS "${file}" AND
          NOT IS_SYMLINK "${file}")
@@ -176,7 +176,7 @@ if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMP
 endif()
 
 if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphonesimulator/bin" TYPE DIRECTORY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Release/NJLIGameEngine.app" USE_SOURCE_PERMISSIONS)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/platform/ios/11.2/Release-iphoneos/bin" TYPE DIRECTORY FILES "/Users/jamesfolk/Documents/NJLI/Engine/projects/ios_Xcode/build/Release/NJLIGameEngine.app" USE_SOURCE_PERMISSIONS)
 endif()
 
 if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
